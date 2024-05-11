e97241469325e1ceae65dd3a51de954793f09aee
GustavoBarbos4 <gustavo.sena0@gmail.com>
2024-05-11 14:44:33 -0300

    refactor: 💡 new style for details modal

--- src/screens/Persons/components/ModalFoundPerson/ModalFoundPerson.styles.tsx
+++ src/screens/Persons/components/ModalFoundPerson/ModalFoundPerson.styles.tsx
@@ -1,36 +1,55 @@
 import styled from "styled-components";
 
 export const ModalContainer = styled.div`
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .columns-container {
     display: flex;
     flex-direction: column;
-    align-items: center;
-    
-    .columns-container {
+    margin-top: 20px;
+
+    .column {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      gap: 20px;
+      justify-content: space-around;
+      padding: 10px;
+
+      img {
+        max-width: 100%;
+        max-height: 400px;
+        width: 400px;
+        border-radius: 8px;
+      }
+
+      > div {
         display: flex;
-        flex-direction: row;
-        
-        .column {
-            display: flex;
-            flex-direction: column;
-            width: 100%;
-            justify-content: space-around;
-            padding: 10px;
+        flex-direction: column;
+        gap: 10px;
+        background-color: #f5f5f5;
+        padding: 10px;
+        border-radius: 8px;
+      }
 
-            img {
-                max-width: 100%;
-                max-height: 400px;
-                width: 400px;
-                border-radius: 8px;
-            }
-            
-            .box {
-                border: 1px solid black;
-                padding: 5px;
-            }
+      label {
+        font-size: 1.2rem;
+        font-weight: bold;
+      }
+
+      .input {
+        border-radius: 8px;
+        word-wrap: break-word;
+        line-height: 1.5;
+      }
     }
-    
 
-}
+    @media (min-width: 768px) {
+      flex-direction: row;
+    }
+  }
 `;
--- src/screens/Persons/components/ModalFoundPerson/ModalFoundPerson.tsx
+++ src/screens/Persons/components/ModalFoundPerson/ModalFoundPerson.tsx
@@ -18,22 +18,21 @@ const ModalFoundPerson = ({ foundPerson, onClose }: ModalFoundPersonProps) => {
         <h1>{foundPerson.name ?? "Sem nome"}</h1>
         <div className="columns-container">
           <div className="column">
-            {foundPerson.imageLink && <img src={foundPerson.imageLink} alt="Imagem da pessoa"
-             />}
+            {foundPerson.imageLink && (
+              <img src={foundPerson.imageLink} alt="Imagem da pessoa" />
+            )}
           </div>
           <div className="column">
             <div>
               <label htmlFor="description">Descrição Física:</label>
-              <div id="description" className="box">
+              <div id="description" className="input">
                 {foundPerson.description ?? "Sem descrição"}
               </div>
             </div>
             <div>
               <label htmlFor="contact">Contato:</label>
-              <div id="contact" className="box">
-                {`${foundPerson.foundBy.email}
-                                     ${foundPerson.foundBy.contacts}
-                                    `}
+              <div id="contact" className="input">
+                {`${foundPerson.foundBy.email} ${foundPerson.foundBy.contacts}`}
               </div>
             </div>
           </div>
